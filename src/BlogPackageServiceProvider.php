f4efaebe0dbb9467fbe1ccb4a1ed539b501f1d8a
Sigma247 <khalildida93@gmail.com>
2022-04-20 01:34:05 +0000

    first commit

---
+++ src/BlogPackageServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Deidax\BlogPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogPackageServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+    $this->app->bind('calculator', function($app) {
+      return new Calculator();
+    });
+  }
+
+  public function boot()
+  {
+    //
+  }
+}
